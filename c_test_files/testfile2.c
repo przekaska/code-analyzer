b6ac159e023739bc33496651d77db64f9448f1e0
przekaska <kacper.blaszkowski.12@gmail.com>
2023-04-28 20:03:04 +0200

    Added test files

---
+++ c_test_files/testfile2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define SOMETHING 1
+
+int global = 1;
+
+int main(){
+    float something;
+    for(int i = 0; i < 10; i++)
+        printf("Something");
+    char hello = 'H';
+    char ghetto;
+    something = 3;
+}
